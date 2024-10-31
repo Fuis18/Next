da574246f7cba0de6cf22d5bae0aceb8d9ebad5e
Fuis18 <96286214+Fuis18@users.noreply.github.com>
2024-10-31 14:12:38 -0500

    Update Time.jsx

--- app/components/NavBar/Time.jsx
+++ app/components/NavBar/Time.jsx
@@ -7,21 +7,12 @@ const addZeros = (n) => {
 };
 
 export default function Time() {
-  const [time, setTime] = useState(new Date());
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-
-    return () => clearInterval(interval); // Cleanup interval on component unmount
-  }, []);
 
   return (
     <div className="time">
-      <div className="hora">{addZeros(time.getHours())}</div>
+      <div className="hora">{addZeros("00")}</div>
       <div>:</div>
-      <div className="min">{addZeros(time.getMinutes())}</div>
+      <div className="min">{addZeros("00")}</div>
     </div>
   );
 }
